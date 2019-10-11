a1599a02b9fadb4e0eb56c5ea5f77c1fbc1ca07f
Kunto Mustiko <kuntomustiko@gmail.com>
2019-10-11 13:13:48 +0700

    6

--- app/src/main/java/com/minara/kirana/myticketsaya/GetStartedActivity.java
+++ app/src/main/java/com/minara/kirana/myticketsaya/GetStartedActivity.java
@@ -2,13 +2,27 @@ package com.minara.kirana.myticketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btnSignIn = (Button) findViewById(R.id.btn_signIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoSignIn = new Intent(GetStartedActivity.this, SignInActivity.class);
+                startActivity(gotoSignIn);
+            }
+        });
     }
 }
