20df6171668c35c05554db7aca8dbeb13e41f057
Kunto Mustiko <kuntomustiko@gmail.com>
2019-10-12 13:49:34 +0700

    9b

---
+++ app/src/main/java/com/minara/kirana/myticketsaya/MyTIcketDetailActivity.java
@@ -0,0 +1,14 @@
+package com.minara.kirana.myticketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyTIcketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ticket_detail);
+    }
+}
