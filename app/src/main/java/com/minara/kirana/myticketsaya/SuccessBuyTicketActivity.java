ebb7356692a7246da4ab3fd188317fc7e1d80b79
Kunto Mustiko <kuntomustiko@gmail.com>
2019-10-12 13:12:59 +0700

    8d

---
+++ app/src/main/java/com/minara/kirana/myticketsaya/SuccessBuyTicketActivity.java
@@ -0,0 +1,14 @@
+package com.minara.kirana.myticketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessBuyTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_buy_ticket);
+    }
+}
